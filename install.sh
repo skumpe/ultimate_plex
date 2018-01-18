8ebc181b8386d43fea0f655fd0d097bf95c63113
Steven Kumpe <11052180+skumpe@users.noreply.github.com>
2018-01-17 20:09:27 -0500

    Install Radarr deps #8

--- install.sh
+++ install.sh
@@ -10,6 +10,11 @@ message() {
   printf '\E[32m'; echo "$@"; printf '\E[0m'
 }
 
+#Make infos white
+info() {
+  echo "$@"
+}
+
 #Check if run as root or with sudo, error and exit if not
 if [[ $EUID -gt 0 ]]; then
             error "This script MUST be run using sudo or as the root user"
@@ -20,24 +25,29 @@ if [[ $EUID -gt 0 ]]; then
 fi
 
 #Add the plex user, for now with no password to prevent remote logins, may change in future
-echo "Adding plex user"
+info "Adding plex user"
 /usr/sbin/useradd -m -s /bin/bash plex
 message "Successfully added Plex user"
 
 #add the keys and repos, test to see if silent! (its probably not)
-echo "Adding needed repos and corresponding keys"
+info "Adding needed repos and corresponding keys"
 /usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC > /dev/null 2>&1
-echo "deb http://apt.sonarr.tv/ master main" > /etc/apt/sources.list.d/sonarr.list
+info "deb http://apt.sonarr.tv/ master main" > /etc/apt/sources.list.d/sonarr.list
 message "Successfully added Sonarr repo to apt"
 /usr/bin/add-apt-repository -y ppa:jcfp/nobetas > /dev/null 2>&1
 message "Successfully added SABnzbd repo to apt"
 
 #update apt repo data, install Sonarr and change ownership to the plex user (dont freak out about the package name, Sonarr used to be NZBDrone)
-echo "Installing Sonarr and dependencies..."
+info "Installing Sonarr and dependencies..."
 /usr/bin/apt-get update > /dev/null 2>&1
 /usr/bin/apt-get install nzbdrone -y > /dev/null 2>&1
 /bin/chown -R plex: /opt/NzbDrone
 message "Sonarr installed successfully"
+
+#And now install Radarr dependencies
+info "Installing Radarr dependencies..."
+/usr/bin/apt-get install libmono-cil-dev curl mediainfo -y > /dev/null 2>&1
+message "Radarr deps installed successfully"
 
 #Copy the systemd service file for Sonarr in to place, enable during startup, and start the service
 cp -fr ./sonarr.service /etc/systemd/system/
